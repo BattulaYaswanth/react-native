f27048f58ed5d4d3f7d7277203cb0caedc91ad78
Yaswanth <yashmanic96@gmail.com>
2026-07-05 18:08:55 +0530

    Setup and authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,44 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Slot, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import {useAuth,AuthProvider} from "@/components/auth";
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+function InitialLayout() {
+  const { userToken, isLoading } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+  useEffect(() => {
+    if (isLoading) return;
+
+    // Check if user is currently inside the (auth) group
+    const inAuthGroup = segments[0] === '(auth)';
+
+    if (!userToken && !inAuthGroup) {
+      // Redirect to the sign-in page if not authenticated
+      router.replace('/(auth)/login');
+    } else if (userToken && inAuthGroup) {
+      // Redirect to the main app if authenticated
+      router.replace('/(home)');
+    }
+  }, [userToken, isLoading, segments, router]);
+
+  if (isLoading) {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+    );
+  }
+
+  return <Slot />;
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <AuthProvider>
+        <InitialLayout />
+      </AuthProvider>
   );
 }
